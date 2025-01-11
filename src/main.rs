a42b2d0a39e8b49c8f169791b2327331acb9d05e
annieversary <annie@versary.town>
2025-01-11 12:32:39 +0000

    good_lp

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::path::Path;
 
 use ddragon::parse_item_json;
+use good_lp::{variable, variables, IntoAffineExpression, Solution as _, SolverModel};
 use items::ddragon_to_items;
-use microlp::{ComparisonOp, OptimizationDirection, Problem, Solution, Variable};
 
 use crate::items::Item;
 
@@ -11,64 +11,73 @@ mod items;
 
 fn main() {
     let items = parse_item_json(Path::new("./item.json")).unwrap();
-    let mut items = ddragon_to_items(&items);
+    let items = ddragon_to_items(&items);
 
     println!(
         "Found {} items that contribute to desired stats",
         items.len()
     );
 
-    // TODO find the items by name and not by id
-
-    // nashor's tooth makes this slow as fuck
-    items.remove(51);
-
-    // Kraken Slayer
-    items.remove(102);
-
-    println!("Ignorning Nashor's Tooth and Kraken Slayer");
-
-    microlp(&items);
+    good_lp(&items);
 }
 
-fn microlp(items: &[Item]) {
-    let mut problem = Problem::new(OptimizationDirection::Minimize);
-    let vars = items
+fn good_lp(items: &[Item]) {
+    let mut vars = variables! {};
+
+    let mut cost = 0.into_expression();
+
+    let mut ad = 0.into_expression();
+    let mut ap = 0.into_expression();
+    let mut asp = 0.into_expression();
+    // There are a total of 6 slots
+    let mut slots = 0.into_expression();
+
+    let items = items
         .iter()
         .map(|item| {
-            (
-                problem.add_integer_var(item.cost as _, (0, item.limit as _)),
-                item,
-            )
+            let var = vars.add(variable().integer().min(0).max(item.limit));
+
+            ad += var * item.ad;
+            ap += var * item.ap;
+            asp += var * item.asp;
+            slots += var * 1;
+            cost += var * item.cost;
+
+            (var, item)
         })
         .collect::<Vec<_>>();
 
-    // TODO limit number of items bc of slots
-    let slots_constraints = vars.iter().map(|(var, _item)| (*var, 1.0));
-    problem.add_constraint(slots_constraints, ComparisonOp::Le, 6.0);
-
-    // TODO take into account that requirements scale by level
-    let ad_constraint = vars.iter().map(|(var, item)| (*var, item.ad as f64));
-    problem.add_constraint(ad_constraint, ComparisonOp::Ge, 100.0);
-    let ap_constraint = vars.iter().map(|(var, item)| (*var, item.ap as f64));
-    problem.add_constraint(ap_constraint, ComparisonOp::Ge, 100.0);
-    let asp_constraint = vars.iter().map(|(var, item)| (*var, item.asp as f64));
-    problem.add_constraint(asp_constraint, ComparisonOp::Ge, 100.0);
-
     use std::time::Instant;
     let now = Instant::now();
-    let solution = problem.solve().unwrap();
+
+    let solution = vars
+        .minimise(cost.clone())
+        .using(good_lp::default_solver)
+        .with(ad.geq(100))
+        .with(ap.geq(100))
+        .with(asp.geq(100))
+        .with(slots.leq(6.0))
+        .solve()
+        .unwrap();
+
     let elapsed = now.elapsed();
     println!("Found solution in {:.2?}", elapsed);
 
-    print_solution(&vars, &solution);
+    let cost = solution.eval(cost);
+    let items = items
+        .into_iter()
+        .map(|(var, item)| {
+            let count = solution.value(var).round();
+            (count, item)
+        })
+        .collect::<Vec<_>>();
+    print_solution(&items, cost);
 
     // TODO we can add a new constraint that one of the items doesn't match
     // to get further solutions
 }
 
-fn print_solution(vars: &[(Variable, &Item)], solution: &Solution) {
-    let cost = solution.objective();
+fn print_solution(vars: &[(f64, &Item)], cost: f64) {
     println!("-----");
     println!("Best build");
     println!("Total cost: {}", cost);
@@ -83,9 +92,8 @@ fn print_solution(vars: &[(Variable, &Item)], solution: &Solution) {
     let mut asp = 0;
     let mut ms = 0;
 
-    for (var, item) in vars {
-        let count = solution.var_value_rounded(*var);
-        if count > 0.0 {
+    for (count, item) in vars {
+        if *count > 0.0 {
             println!(
                 "{:<6} {:<24} {:<6} {:<3} {:<3} {:<12} {:<3}",
                 count, item.name, item.cost, item.ad, item.ap, item.asp, item.ms
